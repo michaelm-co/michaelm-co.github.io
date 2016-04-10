243ff2f903793d08f8435b1a9b439f96ec95d377
Michael McLaughlin <michael.mclaughlin@nextthought.com>
2016-04-10 06:32:16 -0500

    attempt 4 swipe

--- js/my-slider.js
+++ js/my-slider.js
@@ -8,17 +8,13 @@ $('.slider-caption').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
  arrows: false,
- swipe: true,
  fade: true,
- touchThreshold: 1,
  asNavFor: '.slider-pane'
 });
 $('.slider-pane').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
- swipe: true,
  speed: 200,
- touchThreshold: 1,
  asNavFor: '.slider-caption',
  focusOnSelect: true
 });
