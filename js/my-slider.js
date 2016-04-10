e49cdd7587d14737d8f9c07be96b6df805f82ed9
Michael McLaughlin <michael.mclaughlin@nextthought.com>
2016-04-10 05:45:29 -0500

    Implemented Slider
    Comprised of two obj. that sync with each other.
    
    .slider-caption
    .slider-pane
    
    Then added two icons for hover.
    
    Addition of the slick folder

---
+++ js/my-slider.js
@@ -0,0 +1,20 @@
+/*
+$(document).ready(function(){
+  $('.slider').slick();
+});
+*/
+
+$('.slider-caption').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.slider-pane'
+});
+$('.slider-pane').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ speed: 200,
+ asNavFor: '.slider-caption',
+ focusOnSelect: true
+});
