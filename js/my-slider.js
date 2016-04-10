aa9acb7fc913af4d8ae84ac9e9facc9d4cfc0e89
Michael McLaughlin <michael.mclaughlin@nextthought.com>
2016-04-10 06:16:59 -0500

    Enable Swipe on Mobile
    set swipe to true

--- js/my-slider.js
+++ js/my-slider.js
@@ -8,12 +8,14 @@ $('.slider-caption').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
  arrows: false,
+ swipe: true,
  fade: true,
  asNavFor: '.slider-pane'
 });
 $('.slider-pane').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
+ swipe: true,
  speed: 200,
  asNavFor: '.slider-caption',
  focusOnSelect: true
